611216eee054cdd79ac4363d69f7136b4b7a1fce
Yevhenii Kurtov <yevhenii@kurtov.pro>
2023-10-23 23:19:53 +0100

    Add function to draw lines

--- assets/js/navigation_grid.js
+++ assets/js/navigation_grid.js
@@ -1,15 +1,17 @@
-import Chart from "chart.js/auto";
+const gridSize = 11; // 11x11 grid
+const cellSize = 40; // Each cell is 40x40 pixels
 
 export const NavigationGrid = {
   mounted() {
     const canvas = this.el;
     const ctx = canvas.getContext("2d");
 
-    const gridSize = 11; // 11x11 grid
-    const cellSize = 40; // Each cell is 40x40 pixels
-
     this.setCanvasDimensions(canvas, gridSize, cellSize);
     this.drawGrid(ctx, gridSize, cellSize);
+
+    this.handleEvent("move", (obj) => {
+      this.move(obj, ctx);
+    });
   },
 
   setCanvasDimensions(canvas, gridSize, cellSize) {
@@ -28,5 +30,54 @@ export const NavigationGrid = {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
+  },
+
+  drawDot(ctx, x, y) {
+    const dotRadius = 5;
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(x, y, dotRadius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = "black";
+  },
+
+  move({ current_position, direction, num_cells }, ctx) {
+    // Convert current position to pixels
+    const startX = current_position.x * cellSize;
+    const startY = current_position.y * cellSize;
+
+    let endX, endY;
+
+    // Determine the end coordinates based on the direction and number of cells
+    switch (direction) {
+      case "up":
+        endX = startX;
+        endY = startY - num_cells * cellSize;
+        break;
+      case "right":
+        endX = startX + num_cells * cellSize;
+        endY = startY;
+        break;
+      case "down":
+        endX = startX;
+        endY = startY + num_cells * cellSize;
+        break;
+      case "left":
+        endX = startX - num_cells * cellSize;
+        endY = startY;
+        break;
+      default:
+        console.error("Invalid direction");
+        return;
+    }
+
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    this.drawLine(ctx, startX, startY, endX, endY);
+    this.drawDot(ctx, endX, endY);
+
+    // reset to default style
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
   },
 };
--- lib/tempus_web/live/sample.ex
+++ lib/tempus_web/live/sample.ex
@@ -1,42 +1,67 @@
 defmodule TempusWeb.SampleLive do
   use TempusWeb, :live_view
-  
-  
-def render(assigns) do
-  ~H"""
-  <div class="flex p-8 bg-gray-100">
-     <!-- Graph Section -->
-     <div class="flex-1 bg-white border p-6 mr-4 rounded shadow-lg">
-        <div id="chart-container"  >
-            <canvas id="myChart" phx-hook="NavigationGrid"></canvas>
-        </div>
-         
-       
-     </div>
-     
-     <!-- Table Section -->
-     <div class="w-1/4 bg-white border p-4 rounded shadow-lg">
-        <h2 class="text-xl font-semibold mb-4">Event Based State Transitions</h2>
-        <table class="w-full">
-           <thead>
-              <tr class="border-b">
-                 <th class="pb-2">Name</th>
-                 <th class="pb-2">Direction</th>
-                 <th class="pb-2">Transaction Time</th>
-              </tr>
-           </thead>
-           <tbody>
-              <tr class="border-b">
-                 <td>Stephen</td>
-                 <td><span class="bg-blue-500 text-white px-2 py-1 rounded">↓x5</span></td>
-                 <td>T1</td>
-              </tr>
-              <!-- Add other rows similarly -->
-           </tbody>
-        </table>
-     </div>
-  </div>
 
-  """
+  def render(assigns) do
+    ~H"""
+    <div class="flex p-8 bg-gray-100">
+      <!-- Graph Section -->
+      <div class="flex-1 bg-white border p-6 mr-4 rounded shadow-lg">
+        <div id="chart-container">
+          <canvas id="myChart" phx-hook="NavigationGrid"></canvas>
+        </div>
+      </div>
+      <!-- Table Section -->
+      <div class="w-2/5 bg-white border p-4 rounded shadow-lg">
+        <h2 class="text-xl font-semibold mb-4">Event Based State Transitions</h2>
+        <form phx-submit="move">
+          <select class="w-auto" name="direction">
+            <option value="up">Up</option>
+            <option value="down">Down</option>
+            <option value="left">Left</option>
+            <option value="right">Right</option>
+          </select>
+
+          <input
+            type="number"
+            class="w-40"
+            name="num_cells"
+            min="1"
+            max="5"
+            placeholder="Number of Cells"
+            required="true"
+          />
+
+          <.button type="submit">Move</.button>
+        </form>
+
+        <table class="w-full">
+          <thead>
+            <tr class="border-b">
+              <th class="pb-2">Name</th>
+              <th class="pb-2">Direction</th>
+              <th class="pb-2">Transaction Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b">
+              <td>Stephen</td>
+              <td><span class="bg-blue-500 text-white px-2 py-1 rounded">↓x5</span></td>
+              <td>T1</td>
+            </tr>
+            <!-- Add other rows similarly -->
+          </tbody>
+        </table>
+      </div>
+    </div>
+    """
+  end
+
+  def handle_event("move", %{"direction" => direction, "num_cells" => num_cells}, socket) do
+    {:noreply,
+     push_event(socket, "move", %{
+       direction: direction,
+       num_cells: num_cells,
+       current_position: %{x: 0, y: 0}
+     })}
   end
 end
