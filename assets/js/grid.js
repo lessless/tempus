6515d3c95b2d0673e736cab30b8bfbdb1b06bad0
Yevhenii Kurtov <yevhenii@kurtov.pro>
2023-10-24 20:30:51 +0100

    Extract grid.js

---
+++ assets/js/grid.js
@@ -0,0 +1,30 @@
+import Canvas from "./canvas.js";
+
+export default class Grid {
+  constructor(el, gridSize, cellSize) {
+    this.gridSize = gridSize; // 11x11 grid
+    this.cellSize = cellSize; // Each cell is 40x40 pixels
+    this.labelOffset = 10;
+    this.canvas = el;
+    this.ctx = el.getContext("2d");
+  }
+
+  draw() {
+    this.setCanvasDimensions();
+    Canvas.drawGrid(this.ctx, this.gridSize, this.cellSize);
+  }
+
+  setCanvasDimensions() {
+    this.canvas.height = this.canvas.width = this.gridSize * this.cellSize;
+  }
+
+  markStartingPoint(position) {
+    Canvas.drawNumberInCircle(
+      this.ctx,
+      position.x * this.cellSize,
+      position.y * this.cellSize,
+      1,
+      { color: "aquamarine" }
+    );
+  }
+}
--- assets/js/hooks/transportation_grid.js
+++ assets/js/hooks/transportation_grid.js
@@ -1,76 +1,55 @@
-const gridSize = 11; // 11x11 grid
-const cellSize = 40; // Each cell is 40x40 pixels
-
-import Canvas from "../canvas.js";
+import Grid from "../grid.js";
 
 export const TransportationGrid = {
   mounted() {
-    const canvas = this.el;
-    const ctx = canvas.getContext("2d");
-
-    this.setCanvasDimensions(canvas, gridSize, cellSize);
-    Canvas.drawGrid(ctx, gridSize, cellSize);
-    this.markStartingPoint(ctx, JSON.parse(this.el.dataset.startingPosition));
+    const grid = new Grid(this.el, 11, 40);
+    grid.draw();
+    grid.markStartingPoint(JSON.parse(this.el.dataset.startingPosition));
 
     this.handleEvent("move", (obj) => {
       this.move(obj, ctx);
     });
   },
 
-  setCanvasDimensions(canvas, gridSize, cellSize) {
-    canvas.width = gridSize * cellSize;
-    canvas.height = gridSize * cellSize;
-  },
-
-  markStartingPoint(ctx, position) {
-    Canvas.drawNumberInCircle(
-      ctx,
-      position.x * cellSize,
-      position.y * cellSize,
-      1,
-      { color: "aquamarine" }
-    );
-  },
-
-  move({ current_position, direction, num_cells }, ctx) {
-    // Convert current position to pixels
-    const startX = current_position.x * cellSize;
-    const startY = current_position.y * cellSize;
-
-    let endX, endY;
-
-    // Determine the end coordinates based on the direction and number of cells
-    switch (direction) {
-      case "up":
-        endX = startX;
-        endY = startY - num_cells * cellSize;
-        break;
-      case "right":
-        endX = startX + num_cells * cellSize;
-        endY = startY;
-        break;
-      case "down":
-        endX = startX;
-        endY = startY + num_cells * cellSize;
-        break;
-      case "left":
-        endX = startX - num_cells * cellSize;
-        endY = startY;
-        break;
-      default:
-        console.error("Invalid direction");
-        return;
-    }
-
-    Canvas.drawLine(
-      ctx,
-      { startX, startY, endX, endY },
-      { width: 5, color: "aquamarine" }
-    );
-    Canvas.drawDot(ctx, endX, endY, "red");
-
-    // reset to default style
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "black";
-  },
+  //   move({ current_position, direction, num_cells }, ctx) {
+  //     // Convert current position to pixels
+  //     const startX = current_position.x * cellSize;
+  //     const startY = current_position.y * cellSize;
+  //
+  //     let endX, endY;
+  //
+  //     // Determine the end coordinates based on the direction and number of cells
+  //     switch (direction) {
+  //       case "up":
+  //         endX = startX;
+  //         endY = startY - num_cells * cellSize;
+  //         break;
+  //       case "right":
+  //         endX = startX + num_cells * cellSize;
+  //         endY = startY;
+  //         break;
+  //       case "down":
+  //         endX = startX;
+  //         endY = startY + num_cells * cellSize;
+  //         break;
+  //       case "left":
+  //         endX = startX - num_cells * cellSize;
+  //         endY = startY;
+  //         break;
+  //       default:
+  //         console.error("Invalid direction");
+  //         return;
+  //     }
+  //
+  //     Canvas.drawLine(
+  //       ctx,
+  //       { startX, startY, endX, endY },
+  //       { width: 5, color: "aquamarine" }
+  //     );
+  //     Canvas.drawDot(ctx, endX, endY, "red");
+  //
+  //     // reset to default style
+  //     ctx.lineWidth = 1;
+  //     ctx.strokeStyle = "black";
+  //   },
 };
