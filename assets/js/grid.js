cfb3d12063592e8b4f78e5169b8440b1127f03e0
Yevhenii Kurtov <yevhenii@kurtov.pro>
2023-10-24 20:37:59 +0100

    Mark variables as  private

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,24 +1,29 @@
 import Canvas from "./canvas.js";
 
 export default class Grid {
+  #gridSize;
+  #cellSize;
+  #labelOffset = 10;
+  #canvas;
+  #ctx;
+
   constructor(el, gridSize, cellSize) {
-    this.gridSize = gridSize; // 11x11 grid
-    this.cellSize = cellSize; // Each cell is 40x40 pixels
-    this.labelOffset = 10;
-    this.canvas = el;
-    this.ctx = el.getContext("2d");
+    this.#gridSize = gridSize; // 11x11 grid
+    this.#cellSize = cellSize; // Each cell is 40x40 pixels
+    this.#canvas = el;
+    this.#ctx = el.getContext("2d");
   }
 
   draw() {
-    this.canvas.height = this.canvas.width = this.gridSize * this.cellSize;
-    Canvas.drawGrid(this.ctx, this.gridSize, this.cellSize);
+    this.#canvas.height = this.#canvas.width = this.#gridSize * this.#cellSize;
+    Canvas.drawGrid(this.#ctx, this.#gridSize, this.#cellSize);
   }
 
   markStartingPoint(position) {
     Canvas.drawNumberInCircle(
-      this.ctx,
-      position.x * this.cellSize,
-      position.y * this.cellSize,
+      this.#ctx,
+      position.x * this.#cellSize,
+      position.y * this.#cellSize,
       1,
       { color: "aquamarine" }
     );
